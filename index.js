6ac52fa14841982308615162b8de9b340a756212
JuanAnaya-Kas <74438778+JuanAnaya-Kas@users.noreply.github.com>
2022-02-07 18:47:59 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log('Hola mundo');
+
+//Tidpos de datos en javascript
+//STING (cadena de caracteres. ejemplos: 'a' 'hola mundo' 'jake el perro')
+//Boolean (true o false)
+//Null : es un valor y su valor es nulo
+//Number (cualquier numero)
+//Undefined. (algo que no esta definido)
+//object: objeto (puede contener cualquier tipo de dato en una estructura)
+
+//DECLARAICON DE VARIALBES (VAR, LET O CONST)
+
+//var miPrimeraVarible = 'lala';
+
+//MUTABLILIDAD de las variables es cuando se puede cambiar el valor de una variable
+
+let miPrimeraVarible = 'Mi primera variable';
+
+miPrimeraVarible = 'Esto ha cambiado';
+
+// bolleanos
+let miBoleano = true;
+let miBoleano2 = false;
+
+//Buena practica el nombre de la variable debe empezar con una letra y concortar con el valor que ablerga
+ 
+let miNumero = 0;
+let miNumero2 = 12;
+let miNumero3 = -258;
+
+console.log(miNumero, miNumero2, miBoleano, miPrimeraVarible);
+
+let undef
+console.log(undef);
+
+let nulo = null;
+
+console.log(nulo);

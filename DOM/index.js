35e9190f7304ca866b0cd2be99a5a095a2ee66f2
JuanAnaya-Kas <juanpabloanaya2012@hotmail.com>
2022-03-09 00:10:04 -0500

    Update for class #103

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,5 @@
-console.log('INTRO A DOM')
+//wondow.onload es un evento que se dispara cuando la pagina se carga
+window.onload = () => {
+    const parrafo = document.getElementById('text')
+    console.log(parrafo.innerText)
+}

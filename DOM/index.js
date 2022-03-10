8f3ddd47e3e1a6a65f62f8e1a7da29aa59c76909
JuanAnaya-Kas <juanpabloanaya2012@hotmail.com>
2022-03-09 20:11:57 -0500

    Update for class #103

--- DOM/index.js
+++ DOM/index.js
@@ -1,5 +1,7 @@
 //wondow.onload es un evento que se dispara cuando la pagina se carga
 window.onload = () => {
     const parrafo = document.getElementById('text')
-    console.log(parrafo.innerText)
+    //console.log(parrafo.innerText)
+    //.innerText es para obtener el texto del elemento y cambiarlo
+    parrafo.innerText = 'Texto cambiado'
 }

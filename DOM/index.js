1bd199205168ee22b048d85ac18e223dbf8c2473
JuanAnaya-Kas <juanpabloanaya2012@hotmail.com>
2022-03-21 16:40:06 -0500

    Update for class #116

--- DOM/index.js
+++ DOM/index.js
@@ -1,5 +1,28 @@
 //wondow.onload es un evento que se dispara cuando la pagina se carga
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
+const render = () => {
+    const todoList = document.getElementById('todo-list');
+    //El metodo map crea un nuevo array con los resultados de la funcion que se le pasa
+    const todosTemplate = todos.map(t =>  '<li>' + t + '</li>');
+    //El metodo join une los elementos de un array en un string
+    todoList.innerHTML = todosTemplate.join('');
+    const elementos = document.querySelectorAll('#todo-list li')
+    elementos.forEach((elemento, i) => {
+        elemento.addEventListener('click', () =>{
+            elemento.parentNode.removeChild(elemento)
+            todos.splice(i, 1)
+            actualizaTodo(todos);
+            render();
+        })
+    })
+}
+
+const actualizaTodo = (todos) => {
+    const todoStings = JSON.stringify(todos);
+    localStorage.setItem('todos', todoStings)
+}
 window.onload = () => {
+    render();
     //const parrafo = document.getElementById('text')
     //console.log(parrafo.innerText)
     //.innerText es para obtener el texto del elemento y cambiarlo
@@ -13,6 +36,10 @@ window.onload = () => {
         const todo = document.getElementById('todo');
         const todoText = todo.value;
         todo.value = '';
-        console.log(todoText);
+        todos.push(todoText);
+        actualizaTodo(todos);
+        //console.log(todoText);
+        render();
     }
 }
+
